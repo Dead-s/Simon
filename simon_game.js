776501a44deb9420b9fdd1d8e80304be51377c76
Dead-s <120720557+Dead-s@users.noreply.github.com>
2023-01-07 18:36:48 +0530

    Update simon_game.js

--- simon_game.js
+++ simon_game.js
@@ -10,6 +10,7 @@ $(document).ready(() => {
         });
         nextSequence();
     });
+
     $(".btn").click((e) => {
         var userChosenColour = $(e.target).attr("id");
         console.log(userChosenColour);
@@ -19,7 +20,9 @@ $(document).ready(() => {
         pressed(userChosenColour);
         check(userSelectedcolors.length - 1);
     });
+
     function nextSequence() {
+        $("#level-title").text("Level " + level);
         var random = Math.floor(Math.random() * 4);
         console.log(random);
         let audioforcolor = colors[random];
@@ -28,29 +31,28 @@ $(document).ready(() => {
         randomseq.push(audioforcolor);
 
     }
+
     function playAudio(name) {
         var audio = new Audio(name + '.mp3');
         audio.play();
-        console.log("outside : " + randomseq);
     }
+
     function check(userChoice) {
         let users = userSelectedcolors, random = randomseq, valid = false;
-
         console.log("random : " + randomseq);
         console.log("users color : " + userSelectedcolors);
 
         if (random[userChoice] === users[userChoice]) {
             if (users.length === random.length) {
                 setTimeout(() => {
-                    console.log("nextseq");
-                    console.log("users color length : " + userSelectedcolors.length);
+                    // console.log("nextseq");
+                    // console.log("users color length : " + userSelectedcolors.length);
                     userSelectedcolors = [];
-                    console.log("users color length after clear : " + userSelectedcolors.length);
+                    // console.log("users color length after clear : " + userSelectedcolors.length);
                     nextSequence();
                 }, 800);
             }
         } else {
-            console.log("wrong");
             $("body").css("background-color", "red");
             setTimeout(() => {
                 $("body").css("background-color", "#011F3F");
