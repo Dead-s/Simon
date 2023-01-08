e67f49e1eb8aa8541ecb672e29b52a13414ac01c
Dead-s <120720557+Dead-s@users.noreply.github.com>
2023-01-09 00:29:49 +0530

    Update simon_game.js

--- simon_game.js
+++ simon_game.js
@@ -1,36 +1,65 @@
-
+var colors = ["red", "blue", "yellow", "green"];
+var randomseq = [];
+var userSelectedcolors = [];
+var level = 0;
+var taps = 0;
 $(document).ready(() => {
-    var colors = ["red", "blue", "yellow", "green"];
-    var randomseq = [];
-    var userSelectedcolors = [];
-    var level = 0;
     $("#start").click(() => {
         $("#start").css({
             "visibility": "hidden"
         });
+        $(".mode-div").css("visibility", "hidden");
         nextSequence();
     });
 
     $(".btn").click((e) => {
         var userChosenColour = $(e.target).attr("id");
-        console.log(userChosenColour);
+//        console.log(userChosenColour);
         userSelectedcolors.push(userChosenColour);
         playAudio(userChosenColour);
         $(userChosenColour).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
         pressed(userChosenColour);
+        taps--;
+        $("#taps-count").text("Taps remaining : " + taps);
         check(userSelectedcolors.length - 1);
+
     });
 
     function nextSequence() {
         level++;
+        taps = level;
         $("#level-title").text("Level " + level);
         var random = Math.floor(Math.random() * 4);
-        console.log(random);
+        $(".btn").css("pointer-events", "none");
+        $("#taps-count").text("Wait for your turn!");
         let audioforcolor = colors[random];
-        $("#" + audioforcolor + "").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
-        playAudio(audioforcolor);
         randomseq.push(audioforcolor);
-
+        if ($("#easy-mode").prop('checked') === true) {
+//            alert("Easy mode!");
+            randomseq.forEach((color, index) => {
+                setTimeout(() => {
+                    $("#" + color + "").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+                    playAudio(color);
+                    if (index === randomseq.length - 1) {
+                        $("#taps-count").text("Your turn!");
+                        setTimeout(() => {
+                            $(".btn").css("pointer-events", "auto");
+                            $("#taps-count").text("Taps remaining : " + taps);
+                        }, 1000);
+                    }
+                }, (index + 1) * 600);
+            });
+        }
+        if ($("#hard-mode").prop('checked') === true) {
+//            alert("Hard mode!");
+            $("#" + audioforcolor + "").fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
+            playAudio(audioforcolor);
+            $("#taps-count").text("Your turn!");
+            setTimeout(() => {
+                $(".btn").css("pointer-events", "auto");
+                $("#taps-count").text("Taps remaining : " + taps);
+            }, 1000);
+        }
     }
 
     function playAudio(name) {
@@ -40,8 +69,8 @@ $(document).ready(() => {
 
     function check(userChoice) {
         let users = userSelectedcolors, random = randomseq, valid = false;
-        console.log("random : " + randomseq);
-        console.log("users color : " + userSelectedcolors);
+//        console.log("random : " + randomseq);
+//        console.log("users color : " + userSelectedcolors);
 
         if (random[userChoice] === users[userChoice]) {
             if (users.length === random.length) {
@@ -61,6 +90,8 @@ $(document).ready(() => {
             playAudio("wrong");
             $("#level-title").text("Game over !");
             $("#start").css("visibility", "visible").text("Restart");
+            $(".mode-div").css("visibility", "visible");
+            $("#taps-count").text(";-;");
             level = 0;
             randomseq = [];
             userSelectedcolors = [];
